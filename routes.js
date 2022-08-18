350ae3c6e6555ef70217cf7a5ab2f58a804bb937
chandan <chandansutradhar2@gmail.com>
2022-08-18 13:06:03 +0530

    added new endpoint for account creation

--- routes.js
+++ routes.js
@@ -2,6 +2,12 @@
 var http = require('http');
 
 http.get('/auth/signup',(req,res)=>{
+
+    res.send('account created successfully')
+})
+
+http.get('/auth/login',(req,res)=>{
+    
     res.send('auth successfull')
 })
 
