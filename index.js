b1e98b9b88036f6f11b08dd8c9e26570254c94d7
chandan <chandansutradhar2@gmail.com>
2022-08-18 12:06:12 +0530

    modified license

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Chandan!');
 }).listen(3000);
+
+console.log('after server has started spawning')
